a0a7f6cca0f169bf708056f5f1ca0a728228b5b9
Sameep Rana <sameep.monsoon@gmail.com>
2023-11-21 14:43:45 +0545

    admin page added

--- components/Elements/AdminNavbar/AdminNavbar.tsx
+++ components/Elements/AdminNavbar/AdminNavbar.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 
 function AdminNav() {
   return (
-    <div className="flex w-auto h-[5rem] justify-start items-center p-3 pt-0 pb-0 border-0 gap-5">
+    <div className="flex w-auto h-[4.5rem] justify-start items-center px-3 pt-0 pb-0 border-0 gap-5 border-b-[1px]">
       <div className="w-[15rem] flex justify-between items-center">
         <Logo to="/" title="Admin" />
         <span className="cursor-pointer   hover:text-[#EDE7F6] text-[#5E35B1] hover:bg-[#5E35B1] h-10 w-10  flex justify-center items-center rounded-[5px]">
@@ -16,7 +16,7 @@ function AdminNav() {
       <div>
         <Input type="text" placeholder="Search"/>
       </div>
-      <NavMenu />
+      {/* <NavMenu /> */}
       <div className="h-full w-auto flex justify-center items-center">
         <Button variant={"text"} size={"md"}>
           Login
---
+++ components/Elements/UserIcon/UserIcon.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const UserIcon = () => {
+  return (
+    <div className="bg-red-600">
+      <Image src={"/"} alt="image " />
+    </div>
+  );
+};
+
+export default UserIcon;
