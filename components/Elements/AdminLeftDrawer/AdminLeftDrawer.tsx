b2c88ebb4591d291c10221cbdeda6c451aa2bf71
Sameep Rana <sameep.monsoon@gmail.com>
2023-11-29 09:17:19 +0545

    admin  sid ebar for small devices added

--- components/Elements/AdminLeftDrawer/AdminLeftDrawer.tsx
+++ components/Elements/AdminLeftDrawer/AdminLeftDrawer.tsx
@@ -7,6 +7,8 @@ import { usePathname } from "next/navigation";
 import CustomToolTip from "../CustomToolTip/CustomToolTip";
 import "../../../Styles/Admin/AdminLeftSider.scss";
 import { colorPallette } from "@/Helpers/Constants/ColourConstants";
+import Logo from "../Logo/Logo";
+import { PiList } from "react-icons/pi";
 const AdminLeftDrawer = () => {
   const { siderState, setSiderState } = useLeftSiderState(
     (state: any) => state
@@ -21,7 +23,7 @@ const AdminLeftDrawer = () => {
           siderState === 2 ? "hidden" : "block"
         }`}
         onClick={setSiderState}
-      />
+      />{" "}
       <nav
         id="adminLeftDrawer"
         className={`adminLeftDrawer dark:bg-[#051114] dark:text-white px-5 overflow-y-scroll h-screen visible transition-width duration-300 ease-in-out ${
@@ -31,6 +33,14 @@ const AdminLeftDrawer = () => {
             ? "w-[6rem]"
             : "w-0 left-[-20rem] pr-0 pl-5 "
         } lg:relative left-0  z-[100] lg:z-0 fixed top-0 lg:flex flex-col items-start justify-start`}>
+        <div className="w-[18rem] flex justify-between items-center flex-nowrap">
+          <Logo to="/admin" title="MARIO" />
+          <span
+            onClick={setSiderState}
+            className="cursor-pointer hover:text-black text-black dark:bg-transparent dark:text-white dark:hover:bg-accent dark:hover:text-black bg-gray-100/60 hover:bg-gray-200/60 h-[2rem] w-[2rem]  flex justify-center items-center rounded-[5px]">
+            <PiList size={20} />
+          </span>
+        </div>
         {adminLeftDrawerList.map((item: any, index: number) => (
           <>
             <ul
