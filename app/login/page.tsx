207821e498b1ab841ea1193168bdde19838841e8
Sameep Rana <sameep.monsoon@gmail.com>
2023-11-30 15:52:24 +0545

    npm packages not installing issue fixed

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
+      quisquam dolorum sed, rerum quos ab, dicta rem unde minus, aliquam nostrum
+      obcaecati nulla ipsam voluptate quis. Reiciendis ea culpa molestias!
+    </div>
+  );
+};
+
+export default page;
