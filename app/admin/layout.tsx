b0c332294cf847b8b7ac15c693b72a1a04d3dd87
Sameep Rana <sameep.monsoon@gmail.com>
2023-12-05 13:30:16 +0545

    minor ui changes

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,20 +5,23 @@ import NavBar from "@/components/Elements/NavBar/NavBar";
 import AdminNav from "@/components/Elements/AdminNavbar/AdminNavbar";
 import AdminPage from "./home/page";
 import AdminLeftDrawer from "@/components/Elements/AdminLeftDrawer/AdminLeftDrawer";
-
+import { Suspense } from "react";
+import Loading from "./loading";
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header>
-        <nav>
-          <AdminNav />
-        </nav>
-      </header>
-      <section
-        className={`flex w-full h-full lg:pr-5 pl-5 pr-5 lg:pl-0 mt-[5.5rem] dark:bg-darkBg `}>
-        <AdminLeftDrawer />
-        {children}
-      </section>
+      <Suspense fallback={<Loading />}>
+        <header>
+          <nav>
+            <AdminNav />
+          </nav>
+        </header>
+        <section
+          className={`flex w-full h-full lg:pr-5 pl-5 pr-5 lg:pl-0 mt-[5.5rem] dark:bg-darkBg `}>
+          <AdminLeftDrawer />
+          {children}
+        </section>
+      </Suspense>
     </>
   );
 }
