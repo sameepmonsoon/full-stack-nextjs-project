491d1bf360b6801b2d08f05f4ca660bbe0034440
Sameep Rana <sameep.monsoon@gmail.com>
2023-12-06 10:33:32 +0545

    button animation added- compoent page removed

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -77,7 +77,9 @@ const LoginPage = () => {
           </div>
 
           <Button
-            className={`w-full bg-darkBg dark:bg-white hover:bg-darkBg dark:hover:bg-white text-white dark:text-darkBg h-10 rounded-md flex justify-center items-center  text-md font-medium capitalize`}>
+            asChild={false}
+            type="button"
+            className={`w-full transform active:scale-75 transition-transform bg-darkBg dark:bg-white hover:bg-darkBg dark:hover:bg-white text-white dark:text-darkBg h-10 rounded-md flex justify-center items-center  text-md font-medium capitalize`}>
             Sign Up
           </Button>
         </div>
--- app/components/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div className='bg-red-900 w-full uppercase'>page</div>
-  )
-}
-
-export default page
