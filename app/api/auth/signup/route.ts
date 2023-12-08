910cbe0759b0e66e1310d233534ddb746a228938
Sameep Monsoon Rana <91055524+sameepmonsoon@users.noreply.github.com>
2023-12-08 08:58:55 +0545

    mongo db conn successful  (#8)
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properl
    
    * console.log removed
    
    * api folder added -- auth yet to start
    
    * dummy api for the post mehtod added successfully
    
    * connection to mongo db successful

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -8,6 +8,32 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: NextApiRequest, res: NextApiResponse) {
   try {
+<<<<<<< HEAD
+    await connect();
+
+    // const body = request.body;
+    // const pass = body.password;
+
+    // console.log(body, pass);
+
+    // if (!pass?.length || pass.length < 5) {
+    //   throw new Error("Password must be at least 5 characters");
+    // }
+
+    // const notHashedPassword = pass;
+    // const salt = bcrypt.genSaltSync(10);
+    // body.password = bcrypt.hashSync(notHashedPassword, salt);
+
+    // const createdUser = await UserAuth.create(body);
+
+    return NextResponse.json(
+      { message: "User created successfully!" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error(error);
+    return res.status(400).json({ error: error.message });
+=======
     const body = request.body;
     const pass = body.password;
 
@@ -26,5 +52,6 @@ export async function POST(request: NextApiRequest, res: NextApiResponse) {
   } catch (error) {
     console.error(error);
     // Handle error response, e.g., res.status(400).json({ error: error.message });
+>>>>>>> 473912b5f4588bd7a2015a715051f264f8d00371
   }
 }
