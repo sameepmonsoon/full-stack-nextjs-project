6c5ae869d9a2960fb811d96382e057e0790f75aa
Sameep Monsoon Rana <91055524+sameepmonsoon@users.noreply.github.com>
2023-12-08 10:02:48 +0545

    user registration api successful  (#9)
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properl
    
    * console.log removed
    
    * api folder added -- auth yet to start
    
    * dummy api for the post mehtod added successfully
    
    * connection to mongo db successful
    
    * user registration api successful

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -6,52 +6,17 @@ import mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function POST(request: NextApiRequest, res: NextApiResponse) {
+export async function POST(request: any, res: NextApiResponse) {
   try {
-<<<<<<< HEAD
     await connect();
 
-    // const body = request.body;
-    // const pass = body.password;
-
-    // console.log(body, pass);
-
-    // if (!pass?.length || pass.length < 5) {
-    //   throw new Error("Password must be at least 5 characters");
-    // }
-
-    // const notHashedPassword = pass;
-    // const salt = bcrypt.genSaltSync(10);
-    // body.password = bcrypt.hashSync(notHashedPassword, salt);
-
-    // const createdUser = await UserAuth.create(body);
-
-    return NextResponse.json(
-      { message: "User created successfully!" },
-      { status: 200 }
-    );
+    const { body, password } = await request.json();
+    const notHashedPassword = password;
+    const salt = bcrypt.genSaltSync(10);
+    body.password = bcrypt.hashSync(notHashedPassword, salt);
+    const createdUser = await UserAuth.create(body);
+    return NextResponse.json({ message: createdUser }, { status: 200 });
   } catch (error: any) {
-    console.error(error);
-    return res.status(400).json({ error: error.message });
-=======
-    const body = request.body;
-    const pass = body.password;
-
-    console.log(body, pass);
-    // if (!pass?.length || pass.length < 5) {
-    //   throw new Error("Password must be at least 5 characters");
-    // }
-    // const notHashedPassword = pass;
-    // const salt = bcrypt.genSaltSync(10);
-    // body.password = bcrypt.hashSync(notHashedPassword, salt);
-    // const createdUser = await UserAuth.create(body);
-    return NextResponse.json(
-      { message: "Email sent successfully! hey aye" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error(error);
-    // Handle error response, e.g., res.status(400).json({ error: error.message });
->>>>>>> 473912b5f4588bd7a2015a715051f264f8d00371
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
