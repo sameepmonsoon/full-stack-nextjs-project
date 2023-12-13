e841cbc6eee442c111263221593e42dae667e659
Sameep Rana <sameep.monsoon@gmail.com>
2023-12-13 12:59:15 +0545

    login auth added

--- app/(authentication)/login/page.tsx
+++ app/(authentication)/login/page.tsx
@@ -1,12 +1,51 @@
+"use client";
+import { validateFormField } from "@/Helpers/validateForm";
 import CustomInputContainer from "@/components/Elements/CutomInputContainer/CustomInputContainer";
 import Logo from "@/components/Elements/Logo/Logo";
 import ThemeButton from "@/components/Elements/ThemeButton/ThemeButton";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useState } from "react";
 const LoginPage = () => {
+  const [formValues, setFormValues] = useState<any>({});
+  const [formErrors, setFormErrors] = useState<any>({});
+
+  const ref = useRef<any>(null);
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    setFormErrors(validateFormField(formValues));
+
+    const hasErrors = validateFormField(formValues);
+    try {
+      if (!hasErrors) {
+        const response = await fetch("api/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formValues),
+        });
+
+        if (response.ok) {
+          console.log("User created successfully");
+        } else {
+          const data = await response.json();
+          console.error("Error:", data.error);
+        }
+      }
+    } catch (error: any) {
+      console.error("Error:", error.message);
+    }
+  }
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
-    <div className="w-full  h-screen flex justify-center items-center p-5 bg-accent  dark:bg-darkBg">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full  h-screen flex justify-center items-center p-5 bg-accent  dark:bg-darkBg">
       <div className="w-[29.5rem] h-auto p-10 flex flex-col justify-start items-center dark:text-white bg-white dark:bg-darkBg rounded-lg gap-2">
         <div className="h-[25%] flex justify-start items-center flex-col gap-2">
           <Logo to="/admin" title="MARIO" />
@@ -28,6 +67,8 @@ const LoginPage = () => {
             containerStyle={"border"}
             label={"Email Address"}
             id="emailAddress"
+            required
+            onChange={handleChange}
           />
           <CustomInputContainer
             size={"default"}
@@ -37,6 +78,8 @@ const LoginPage = () => {
             containerStyle={"border"}
             label={"Password"}
             id="password"
+            required
+            onChange={handleChange}
           />
 
           <div
@@ -77,7 +120,7 @@ const LoginPage = () => {
       <div className="cursor-pointer absolute right-1 top-[40%] mb-auto h-10 w-10 bg-accent flex justify-center items-center rounded-lg">
         <ThemeButton />
       </div>
-    </div>
+    </form>
   );
 };
 
