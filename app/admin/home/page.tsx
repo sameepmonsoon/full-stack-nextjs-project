89684dbe27e45ad7d258b25052dbe3c995312f29
Sameep Rana <sameep.monsoon@gmail.com>
2023-12-15 13:50:20 +0545

    card layout minor imporvements

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -6,26 +6,59 @@ import React from "react";
 function AdminPage() {
   return (
     <div
-      className={`dark:bg-[#030708] dark:text-white bg-accent flex-1 w-auto rounded-xl p-5`}>
+      className={`dark:bg-[#030708] dark:text-white bg-accent flex-1 w-auto rounded-xl p-5 flex `}>
       This is admin page.
-      <div className="h-40 p-2 w-full">
-        <div className="h-40 p-2 w-full">
-          <CustomInputContainer
-            size={"default"}
-            font={"medium"}
-            type="password"
-            inputBorder={"none"}
-            containerStyle={"border"}
-            label={"User Name"}
-            id="userName"
-          />
+      <div className="p-2 w-[20rem] h-full flex flex-col gap-5">
+        <CustomInputContainer
+          size={"default"}
+          font={"medium"}
+          type="password"
+          inputBorder={"none"}
+          containerStyle={"border"}
+          label={"User Name"}
+          id="userName"
+        />
 
-          <CardLayout
-            type={"column"}
-            title={"$100"}
-            detail="Lorem Ipsum is the earning."
-          />
-        </div>
+        <CardLayout
+          type={"row"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning."
+        />
+
+        <CardLayout
+          type={"column"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning."
+        />
+        <CardLayout
+          type={"column"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning.">
+          <div>lorem</div>
+          <div>lorem</div>
+          <div>lorem</div>
+        </CardLayout>
+      </div>
+      <div className="p-2 w-full h-full flex flex-col gap-5">
+        <CardLayout
+          type={"row"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning."
+        />
+
+        <CardLayout
+          type={"column"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning."
+        />
+        <CardLayout
+          type={"column"}
+          title={"$100"}
+          detail="Lorem Ipsum is the earning.">
+          <div>lorem</div>
+          <div>lorem</div>
+          <div>lorem</div>
+        </CardLayout>
       </div>
     </div>
   );
