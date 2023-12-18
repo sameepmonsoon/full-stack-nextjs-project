1aae7049b13e9550f014ec16a1260ad7f6a5ec20
Sameep Monsoon Rana <91055524+sameepmonsoon@users.noreply.github.com>
2023-12-18 09:21:58 +0545

    UI components (#28)
    * components repo page crated
    
    * bread crumbs added
    
    * bread crumbs with different separator added
    
    * bread crumbs with different separator added
    
    * minor improvemensts

--- components/Elements/BreadCrumbs/BreadCrumbs.tsx
+++ components/Elements/BreadCrumbs/BreadCrumbs.tsx
@@ -21,7 +21,30 @@ const BreadCrumbs = ({
   if (separatorType === 3) {
     separator = <CgFormatSlash size={20} />;
   }
+import { VscDash } from "react-icons/vsc";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { CgFormatSlash } from "react-icons/cg";
+const BreadCrumbs = ({
+  gap,
+  separatorType,
+}: {
+  gap: number;
+  separatorType?: number;
+}) => {
+  let separator;
+  if (separatorType === 1) {
+    separator = <VscDash size={25} />;
+  }
+  if (separatorType === 2) {
+    separator = <MdKeyboardArrowRight size={20} />;
+  }
+  if (separatorType === 3) {
+    separator = <CgFormatSlash size={20} />;
+  }
   return (
+    <div className={`w-auto flex justify-start gap-[0px]`}>
+      <StyledBreadCrumb icon={AiTwotoneHome} />
+      <span className="flex justify-center items-center">{separator}</span>
     <div className={`w-auto flex justify-start gap-[0px]`}>
       <StyledBreadCrumb icon={AiTwotoneHome} />
       <span className="flex justify-center items-center">{separator}</span>
@@ -34,8 +57,9 @@ const BreadCrumbs = ({
       <StyledBreadCrumb
         showBackground={true}
         icon={AiTwotoneHome}
-        label="home is where the house is the nepal"
+        label="home is where the house is."
       />
+      <span className="flex justify-center items-center">{separator}</span>
       <span className="flex justify-center items-center">{separator}</span>
     </div>
   );
