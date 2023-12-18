f7b3be1d88dd4a9d6cda27d1ec7f3550c7539094
sameepmonsoon <sameep.monsoon@gmial.com>
2023-12-18 17:15:16 +0545

    git ignore changes and ui imporovements

--- components/Elements/BreadCrumbs/BreadCrumbs.tsx
+++ components/Elements/BreadCrumbs/BreadCrumbs.tsx
@@ -11,27 +11,7 @@ const BreadCrumbs = ({
   gap: number;
   separatorType?: number;
 }) => {
-  let separator;
-  if (separatorType === 1) {
-    separator = <VscDash size={25} />;
-  }
-  if (separatorType === 2) {
-    separator = <MdKeyboardArrowRight size={20} />;
-  }
-  if (separatorType === 3) {
-    separator = <CgFormatSlash size={20} />;
-  }
-import { VscDash } from "react-icons/vsc";
-import { MdKeyboardArrowRight } from "react-icons/md";
-import { CgFormatSlash } from "react-icons/cg";
-const BreadCrumbs = ({
-  gap,
-  separatorType,
-}: {
-  gap: number;
-  separatorType?: number;
-}) => {
-
+  
   let separator;
   if (separatorType === 1) {
     separator = <VscDash size={25} />;
@@ -63,25 +43,7 @@ const BreadCrumbs = ({
         <span className="flex justify-center items-center">{separator}</span>
         <span className="flex justify-center items-center">{separator}</span>
       </div>
-    <div className={`w-auto flex justify-start gap-[0px]`}>
-      <StyledBreadCrumb icon={AiTwotoneHome} />
-      <span className="flex justify-center items-center">{separator}</span>
-    <div className={`w-auto flex justify-start gap-[0px]`}>
-      <StyledBreadCrumb icon={AiTwotoneHome} />
-      <span className="flex justify-center items-center">{separator}</span>
-      <StyledBreadCrumb
-        icon={AiTwotoneHome}
-        label="home"
-        showBackground={true}
-      />
-      <span className="flex justify-center items-center">{separator}</span>
-      <StyledBreadCrumb
-        showBackground={true}
-        icon={AiTwotoneHome}
-        label="home is where the house is."
-      />
-      <span className="flex justify-center items-center">{separator}</span>
-      <span className="flex justify-center items-center">{separator}</span>
+   
     </div>
   );
 };
