105504f15a44b79d13f2800e442fa6f9510815d7
sameepmonsoon <sameep.monsoon@gmail.com>
2023-12-21 12:02:54 +0545

    drag and drop added --not responsive

--- components/Elements/DragAndDrop/DragAndDrop.tsx
+++ components/Elements/DragAndDrop/DragAndDrop.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import "./DragAndDrop.scss";
 
 const DragAndDrop = () => {
   const draggables = document.querySelectorAll(".draggable");
   const containers = document.querySelectorAll(".drag-and-drop-container");
+  const hideClass: any = document.getElementsByClassName("draggable.hide");
 
   draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", () => {
+    draggable.addEventListener("dragstart", (e: any) => {
       draggable.classList.add("dragging");
+      setTimeout(() => {
+        e.target.className += " hide";
+      }, 0);
     });
 
-    draggable.addEventListener("dragend", () => {
+    draggable.addEventListener("dragend", (e: any) => {
       draggable.classList.remove("dragging");
+      e.target.classList.remove("hide");
     });
   });
 
@@ -19,6 +25,8 @@ const DragAndDrop = () => {
       e.preventDefault();
       const afterElement = getDragAfterElement(container, e.clientY);
       const draggable: any = document.querySelector(".dragging");
+
+      console.log(draggable);
       if (afterElement == null) {
         container.appendChild(draggable);
       } else {
@@ -47,20 +55,20 @@ const DragAndDrop = () => {
   }
   //   console.log(draggableDiv, draggableContainerDiv);
   return (
-    <div className="w-full h-[12rem] flex flex-col gap-1">
-      <div className="drag-and-drop-container w-full  flex flex-col gap-2 bg-red-500 h-auto">
+    <div className="w-full flex flex-col gap-1">
+      <div className="drag-and-drop-container w-[20rem]  flex flex-col gap-2 bg-red-500 h-auto p-5 transition-all ease-in-out duration-800">
         <p draggable="true" className="draggable w-full h-10 bg-blue-700">
           Drag me 1
         </p>
-        <p draggable="true" className="draggable w-full h-10 bg-blue-700">
+        <p draggable="true" className="draggable w-full h-10 bg-green-700">
           Drag me 2
         </p>
       </div>
-      <div className="drag-and-drop-container w-full  flex flex-col gap-2 bg-red-700 h-auto">
-        <p draggable="true" className="draggable w-full h-10 bg-blue-700">
+      <div className="drag-and-drop-container w-[20rem]   flex flex-col gap-2 bg-red-700 p-5 h-auto">
+        <p draggable="true" className="draggable w-full h-10 bg-gray-400">
           Drag me 3
         </p>
-        <p draggable="true" className="draggable w-full h-10 bg-blue-700">
+        <p draggable="true" className="draggable w-full h-10 bg-yellow-700">
           Drag me 4
         </p>
       </div>
