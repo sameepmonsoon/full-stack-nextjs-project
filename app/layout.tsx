65f8cfa463aeed469160f6bbc4eaf9a63947ea53
sameepmonsoon <sameep.monsoon@gmail.com>
2023-12-27 09:15:18 +0545

    sonner component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -13,10 +13,8 @@ export default function RootLayout({
   return (
     <html lang="en" className=" h-full">
       <body
-        className={cn(
-          "relative h-full font-sans antialiased",
-          inter.className
-        )}>
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
         <main className="relative flex flex-col h-screen">
           <div className="flex-grow flex-1">{children}</div>
           <Toaster />
