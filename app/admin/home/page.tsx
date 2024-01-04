9b1a3ff7983f72578ad66567bb5d545c1af3690f
sameepmonsoon <sameep.monsoon@gmail.com>
2024-01-04 11:50:11 +0545

    chnages in dashboard side bar

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -1,59 +1,19 @@
 "use client";
 import useRouteHistory from "@/Hooks/useRouteHistory";
-import BreadCrumbs from "@/components/Elements/BreadCrumbs/BreadCrumbs";
 import CardLayout from "@/components/Elements/CardLayout/CardLayout";
 import { CarouselSlider } from "@/components/Elements/Carousels/CarouselSlider/CarouselSlider";
-import CustomInputContainer from "@/components/Elements/CutomInputContainer/CustomInputContainer";
-import ListContainer from "@/components/Elements/ListContainer/ListContainer";
-import LoadingDots from "@/components/Elements/Loading/LoadingDots/LoadingDots";
-import SkeletonCard from "@/components/Elements/Skeleton/SkeletonCard/SkeletonCard";
-import ProtectedRoute from "@/components/HOC/ProtectedRoute";
 import React from "react";
-
 function AdminPage() {
   const routeHistory = useRouteHistory();
-
   return (
-    <div className="flex flex-1">
-      <CarouselSlider />
-      <div className="p-2 w-[20rem] h-full flex flex-col gap-5">
-        <CustomInputContainer
-          size={"default"}
-          font={"medium"}
-          type="password"
-          inputBorder={"none"}
-          containerStyle={"border"}
-          label={"User Name"}
-          id="userName"
-        />
-        <SkeletonCard type={"column"} />
-        <LoadingDots />
-        <BreadCrumbs gap={20} separatorType={5} routeHistory={routeHistory} />
-        <CardLayout
-          type={"row"}
-          title={"$100"}
-          detail="Lorem Ipsum is the earning."
-        />
-
-        <CardLayout
-          type={"column"}
-          title={"$100"}
-          detail="Lorem Ipsum is the earning."
-        />
-        <CardLayout
-          type={"column"}
-          title={"$100"}
-          detail="Lorem Ipsum is the earning."
-        >
-          <div>lorem</div>
-          <div>lorem</div>
-          <div>lorem</div>
-        </CardLayout>
+    <div className="flex flex-1 flex-col gap-2">
+      <div className="p-0 w-full flex gap-5 h-[22rem]">
+        <div className="h-auto w-auto bg-[#D1F1E5] flex-1 rounded-[12px] flex justify-center items-center">
+          a
+        </div>
+        <CarouselSlider />
       </div>
       <div className="p-2 w-full h-full flex flex-col gap-5">
-        {" "}
-        <SkeletonCard type={"column"} />
-        <SkeletonCard type={"row"} />
         <CardLayout
           type={"row"}
           title={"$100"}
@@ -68,101 +28,7 @@ function AdminPage() {
           type={"column"}
           title={"$100"}
           detail="Lorem Ipsum is the earning."
-        >
-          <div>lorem</div>
-          <div>lorem</div>
-          <div>lorem</div>
-        </CardLayout>
-        <div className="w-full flex  gap-2">
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-        </div>
-        <div className="w-full flex  gap-2">
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-        </div>
-        <div className="w-full flex  gap-2">
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-        </div>
-        <div className="w-full flex  gap-2">
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-        </div>
-        <div className="w-full flex  gap-2">
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-          <ListContainer title={"User LIst"} showViewAll={true}>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id at,
-              natus sunt hic veritatis quibusdam iure! Dignissimos maxime est
-              nobis aliquam dolorum ea nostrum, aliquid doloremque facilis quas
-              doloribus ab?
-            </div>
-          </ListContainer>
-        </div>
+        ></CardLayout>
       </div>
     </div>
   );
