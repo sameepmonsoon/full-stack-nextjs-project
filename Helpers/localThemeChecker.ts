7ba7dbd2489ddd159a41cb1c9ff565201569c8bc
sameepmonsoon <sameep.monsoon@gmail.com>
2024-01-04 13:51:33 +0545

    layout issue fixed -- widht not working properly for adminBody

--- Helpers/localThemeChecker.ts
+++ Helpers/localThemeChecker.ts
@@ -3,7 +3,6 @@ export default function localThemeChecker(
   themeMode: string,
   systemTheme: boolean | null
 ) {
-  console.log(themeMode);
   if (themeMode === "D") {
     localStorage.setItem("theme", "D");
     document.documentElement.classList.add("dark");
