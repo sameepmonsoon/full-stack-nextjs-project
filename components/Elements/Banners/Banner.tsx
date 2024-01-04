1e4638524d143face3f23857908d4ad4bba49f07
Sameep Monsoon Rana <91055524+sameepmonsoon@users.noreply.github.com>
2024-01-04 16:51:01 +0545

    Layuout fixed -- carousel added  (#70)
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properly
    
    * pass strength checker working properl
    
    * console.log removed
    
    * api folder added -- auth yet to start
    
    * dummy api for the post mehtod added successfully
    
    * connection to mongo db successful
    
    * user registration api successful
    
    * selection option added fort the role in the sign up section
    
    * enum of role changed in models/schema
    
    * signup page for small dievices improvement -- validation not working properly
    
    * signup page for small dievices improvement -- validation not working properly
    
    * signup page for small dievices improvement -- error message not working properly
    
    * signup page for small dievices improvement -- error message not working properly
    
    * login auth added
    
    * login auth added
    
    * theme not persisting on refresh -- problem
    
    * card container added
    
    * skeleton for card layout
    
    * skeleton for card layout
    
    * skeleton for card layout
    
    * card layout complete with row and column mpode
    
    * card layout minor imporvements
    
    * card layout minor imporvements
    
    * card layout minor imporvements
    
    * card layout minor imporvements
    
    * components repo page crated
    
    * components repo page crated
    
    * components repo page crated
    
    * jwt token added
    
    * validation for login api
    
    * validation for login api
    
    * chantes in components page
    
    * finance tracker is the final project name
    
    * chnages in dashboard side bar
    
    * chnages in dashboard side bar
    
    * layout issue fixed -- widht not working properly for adminBody
    
    * layout issue fixed -- widht not working properly for adminBody
    
    * layout issue fixed
    
    * layout issue fixed

---
+++ components/Elements/Banners/Banner.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="h-auto w-auto bg-white rounded-[12px] flex justify-center items-center px-5 py-2 overflow-hidden gap-1">
+      <div className="h-full flex flex-col justify-center">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem
+        quibusdam voluptas eum, accusantium corrupti neque? Quo, velit eos natus
+        voluptates laudantium mollitia suscipit dolorem. Minus praesentium
+        tenetur est veniam id?
+      </div>
+
+      <div className="min-w-[30%] bg-red-200 h-full flex justify-center items-center overflow-hidden">
+        afdafasas
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
