745d453a37c9609f0a6b646449e161518cf0a485
sameepmonsoon <sameep.monsoon@gmail.com>
2024-01-05 09:43:34 +0545

    minor ui improvements in home page

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -8,15 +8,15 @@ function AdminPage() {
   const routeHistory = useRouteHistory();
   return (
     <div className="flex flex-1 flex-col gap-2">
-      <div className="p-0 w-full flex gap-5 h-[22rem] flex-1 justify-evenly items-stretch">
+      <div className="p-0 w-full flex gap-5 max-h-[22rem] flex-1 justify-evenly items-stretch">
         <Banner />
-        <div className="max-w-[35%] h-full">
+        <div className="max-w-[35%]">
           <CarouselSlider />
         </div>
       </div>
-      <div className="p-2 w-full h-full flex flex-col gap-5">
+      <div className="p-2 w-full h-[10rem] flex gap-5">
         <CardLayout
-          type={"row"}
+          type={"column"}
           title={"$100"}
           detail="Lorem Ipsum is the earning."
         />
@@ -31,6 +31,11 @@ function AdminPage() {
           detail="Lorem Ipsum is the earning."
         ></CardLayout>
       </div>
+      <CardLayout
+        type={"row"}
+        title={"$100"}
+        detail="Lorem Ipsum is the earning."
+      />
     </div>
   );
 }
