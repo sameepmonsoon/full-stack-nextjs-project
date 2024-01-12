37d2f4308bb74100683529604ff21967c190655e
sameepmonsoon <sameep.monsoon@gmail.com>
2024-01-12 12:45:03 +0545

    api added

---
+++ Helpers/Constants/Admin/IncomeConstants.ts
@@ -0,0 +1,13 @@
+const category: any = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+  { value: "Mango", label: "mango" },
+  { value: "Mangoose", label: "mangoose" },
+  { value: "MangoOne", label: "mangoOne" },
+  { value: "MangoTango", label: "mangoTango" },
+  { value: "Apple", label: "apple" },
+  { value: "Choco", label: "choco" },
+];
+
+export { category };
